b8d6470c143de1e1852c94000b2c28b332691bf4
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2026-02-24 16:49:07 +0300

    Create Priority.java

---
+++ project/src/main/java/edu/taskmanager/util/Priority.java
@@ -0,0 +1,11 @@
+package com.taskmanager.util;
+
+/**
+ * Перечисление уровней приоритета задач.
+ */
+public enum Priority {
+    CRITICAL, // Критический приоритет
+    HIGH,     // Высокий приоритет
+    MEDIUM,   // Средний приоритет
+    LOW       // Низкий приоритет
+}
