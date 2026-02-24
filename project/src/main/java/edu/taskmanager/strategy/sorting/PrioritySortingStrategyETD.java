4bfe949eb141cfd58816c1d1b114c90cb7cb9e63
Petlya2000 <135063891+Petlya2000@users.noreply.github.com>
2026-02-24 17:10:08 +0300

    Update PrioritySortingStrategyETD.java

--- project/src/main/java/edu/taskmanager/strategy/sorting/PrioritySortingStrategyETD.java
+++ project/src/main/java/edu/taskmanager/strategy/sorting/PrioritySortingStrategyETD.java
@@ -1,5 +1,7 @@
+package edu.taskmanager.strategy.sorting;
 import java.util.Comparator;
 import java.util.List;
+import edu.taskmanager.util.PriorityETD;
 
 /**
  * Сортировка по приоритету (высокий → низкий).
@@ -7,7 +9,7 @@ import java.util.List;
 public class PrioritySortingStrategyETD implements TaskSortingStrategy {
 
     @Override
-    public List<Task> sort(List<Task> tasks) {
+    public List<TaskETD> sort(List<TaskETD> tasks) {
         // Используем компаратор для сортировки по приоритету
         tasks.sort(Comparator.comparing(task -> task.getPriority().ordinal()));
         return tasks;
