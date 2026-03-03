bf66706c474e1934073f0c4950219f66b6c0d8f1
Stanislav Bazuev <svbazuev@gmail.com>
2026-03-03 18:18:55 +0300

    Small fix PrioritySortingStrategy

--- project/src/main/java/edu/taskmanager/strategy/sorting/PrioritySortingStrategy.java
+++ project/src/main/java/edu/taskmanager/strategy/sorting/PrioritySortingStrategy.java
@@ -1,7 +1,6 @@
 package edu.taskmanager.strategy.sorting;
 
 import edu.taskmanager.model.Task;
-import edu.taskmanager.util.Priority;
 import java.util.Comparator;
 import java.util.List;
 
@@ -13,8 +12,7 @@ public class PrioritySortingStrategy implements TaskSortingStrategy {
     @Override
     public List<Task> sort(List<Task> tasks) {
         // Используем компаратор для сортировки по приоритету
-    tasks.sort(Comparator.comparing((Task task) -> task.getPriority().ordinal()).reversed());
+        tasks.sort(Comparator.comparing((Task task) -> task.getPriority().ordinal()).reversed());
         return tasks;
     }
 }
-
