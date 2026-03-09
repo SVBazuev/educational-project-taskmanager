ac1eba728f74d1f0cbd1801f8b7c6b9985aee60b
Nick <xy4u174@gmail.com>
2026-03-10 01:42:05 +0500

    fix InsertionTaskSortingStrategy & add insertionSort in SortingCommand

--- project/src/main/java/edu/taskmanager/strategy/sorting/InsertionTaskSortingStrategy.java
+++ project/src/main/java/edu/taskmanager/strategy/sorting/InsertionTaskSortingStrategy.java
@@ -1,6 +1,8 @@
 package edu.taskmanager.strategy.sorting;
 
 import edu.taskmanager.model.Task;
+
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -8,35 +10,29 @@ import java.util.List;
  * Сортировка задач по дате выполнения (от ближайшей к самой далёкой).
  */
 
-public class InsertionTaskSortingStrategy implements TaskSortingStrategy {
-
-    @Override
-    public List<Task> sort(List<Task> tasks) {
-        // Компаратор для сортировки по дате выполнения
-        Comparator<Task> comparator = Comparator.comparing(task -> task.getDueDate());
-
-        insertionSort(tasks, comparator);
-
-        return tasks;
-    }
+public class InsertionTaskSortingStrategy {
 
     /**
      * Сортировка вставками (Insertion Sort).
      */
 
-    private void insertionSort(List<Task> tasks, Comparator<Task> comparator) {
-        for (int i = 1; i < tasks.size(); i++) {
-            Task key = tasks.get(i);
+    public static List<Task> insertionSort(List<Task> tasks, Comparator<Task> comparator) {
+
+        List<Task> sortedTasks = new ArrayList<>(tasks);
+
+        for (int i = 1; i < sortedTasks.size(); i++) {
+            Task key = sortedTasks.get(i);
             int j = i - 1;
 
             // Сдвигаем элементы, которые больше key, вправо
-            while (j >= 0 && comparator.compare(tasks.get(j), key) > 0) {
-                tasks.set(j + 1, tasks.get(j));
+            while (j >= 0 && comparator.compare(sortedTasks.get(j), key) > 0) {
+                sortedTasks.set(j + 1, sortedTasks.get(j));
                 j--;
             }
 
             // Вставляем key на правильную позицию
-            tasks.set(j + 1, key);
+            sortedTasks.set(j + 1, key);
         }
+        return sortedTasks;
     }
 }
